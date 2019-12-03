1e4844d9a2a34561a9779d9ae85443ffaacf0643
Wade Butler <wadebutler2@gmail.com>
2019-12-02 21:42:31 -0500

    updated name

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
         // console.log(this.props)
         return (
             <header>
-                <h1>Rocko's Modern Chat</h1>
+                <h1>QuickChat</h1>
                 
             </header>
         )
